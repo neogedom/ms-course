78e5aeba5ddcc6aaa37d1ba8c437f046f69713c6
Vinicius Gomes <neogedom@gmail.com>
2022-09-07 09:56:40 -0400

    Project hr-payroll created

---
+++ hr-payroll/src/test/java/com/neogedom/hrpayroll/HrPayrollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neogedom.hrpayroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrPayrollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
