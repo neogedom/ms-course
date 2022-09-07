11fc021e6380c63f36870e9c86eafa8386cc5420
Vinicius Gomes <neogedom@gmail.com>
2022-09-07 13:34:40 -0400

    Feign client

--- hr-payroll/src/main/java/com/neogedom/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/neogedom/hrpayroll/services/PaymentService.java
@@ -1,30 +1,20 @@
 package com.neogedom.hrpayroll.services;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import com.neogedom.hrpayroll.entities.Payment;
 import com.neogedom.hrpayroll.entities.Worker;
+import com.neogedom.hrpayroll.feignclients.WorkerFeignClients;
 
 @Service
 public class PaymentService {
-    
-    @Value("${hr-worker.host}")
-    private String workerHost;
 
     @Autowired
-    private RestTemplate restTemplate;
+    private WorkerFeignClients workerFeignClient;
 
     public Payment getPayment(Long workerId, Integer days) {
-        Map<String, String> uriVariables = new HashMap<>();
-        uriVariables.put("id", String.valueOf(workerId));
-
-        Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+        Worker worker = workerFeignClient.findById(workerId).getBody();
         return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
 }
